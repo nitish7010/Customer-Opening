02a4316a0f2613c2f7016b0e23b9aab60a638714
nitish.kumar <nitish.kumar@team.cliffex.com>
2022-10-26 00:13:33 +0530

    added url

--- src/Helpers/Axios.js
+++ src/Helpers/Axios.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 import { isAuthenticated } from './helpers'
 
 let url = {
-    dev:"",
+    dev:"http://jklgdev.jktyre.in:9001",
     local:"http://localhost:9001",
 }
 
 const instance = axios.create({
-    baseURL:url.local,
+    baseURL:url.dev,
     headers:{
         Authorization: `Bearer ${isAuthenticated() && isAuthenticated().token}`
     }
